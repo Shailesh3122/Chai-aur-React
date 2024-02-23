f1ebcf161dcebe76d0a5a7600fef27bc4d23e182
Shailesh Kumar <shailesh.31.kumar@gmail.com>
2024-02-23 17:12:44 +0530

    tailwindprops

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <>
+      <h1 class='bg-green-400 text-black p-4 m-50 rounded-xl mb-4'>Tailwind test</h1>
+
+      <Card username="Shailesh" update="Click me"/>
+      <Card username="Sunita" />
+      
+    </>
+  )
+}
+
+export default App
