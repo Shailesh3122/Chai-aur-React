96a588fc23bcb5a2eb24056de870fc08db960e26
Shailesh Kumar <shailesh.31.kumar@gmail.com>
2024-02-23 21:35:46 +0530

    bgChanger

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <h1 class='bg-green-400 text-black p-4 m-50 rounded-xl mb-4'>Tailwind test</h1>
 
-      <Card username="Shailesh" update="Click me"/>
+      <Card username="Shailesh" update="Click me" />
       <Card username="Sunita" />
       
     </>
